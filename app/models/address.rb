344b372e4b2963583b783a568bb115a027ab033d
Rails Udemy Course <railsudemycourse@gmail.com>
2019-06-20 15:05:36 -0700

    Get bootstrap installed

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,8 +3,18 @@ class Address < ApplicationRecord
   include HTTParty
 
   def getHighTemp(zipcode)
-    responseForLocation = HTTParty.get("http://dataservice.accuweather.com/locations/v1/postalcodes/search?apikey=OaXnqpKJOsMuS3vEapwgmBMUS0GG81Yf&q=#{zipcode}")[0]["Key"]
-    responseForHigh = HTTParty.get("http://dataservice.accuweather.com/forecasts/v1/daily/1day/#{responseForLocation}?apikey=OaXnqpKJOsMuS3vEapwgmBMUS0GG81Yf")["DailyForecasts"][0]["Temperature"]["Maximum"]["Value"]
+    responseForLocation = HTTParty.get("http://dataservice.accuweather.com/locations/v1/postalcodes/search?apikey=WrWJozdXHdavGnA2GPSgC2C8x13aatgB&q=#{zipcode}")[0]["Key"]
+    responseForHigh = HTTParty.get("http://dataservice.accuweather.com/forecasts/v1/daily/1day/#{responseForLocation}?apikey=WrWJozdXHdavGnA2GPSgC2C8x13aatgB")["DailyForecasts"][0]["Temperature"]["Maximum"]["Value"]
+  end
+
+  def getLowTemp(zipcode)
+    responseForLocation = HTTParty.get("http://dataservice.accuweather.com/locations/v1/postalcodes/search?apikey=WrWJozdXHdavGnA2GPSgC2C8x13aatgB&q=#{zipcode}")[0]["Key"]
+    responseForLow = HTTParty.get("http://dataservice.accuweather.com/forecasts/v1/daily/1day/#{responseForLocation}?apikey=WrWJozdXHdavGnA2GPSgC2C8x13aatgB")["DailyForecasts"][0]["Temperature"]["Minimum"]["Value"]
+  end
+
+  def getCurrentTemp(zipcode)
+    responseForLocation = HTTParty.get("http://dataservice.accuweather.com/locations/v1/postalcodes/search?apikey=WrWJozdXHdavGnA2GPSgC2C8x13aatgB&q=#{zipcode}")[0]["Key"]
+    responseForCurrent = HTTParty.get("http://dataservice.accuweather.com/currentconditions/v1/#{responseForLocation}?apikey=WrWJozdXHdavGnA2GPSgC2C8x13aatgB")[0]["Temperature"]["Imperial"]["Value"]
   end
 
 
