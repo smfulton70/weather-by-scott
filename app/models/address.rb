12d1dc8d7515671491a98c6da9c50a470b083320
Rails Udemy Course <railsudemycourse@gmail.com>
2019-06-20 20:02:02 -0700

    try hiding api key again

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,19 +1,18 @@
 class Address < ApplicationRecord
   validates :zipcode, presence: true
 
-
   include HTTParty
 
   def getLocationKey(zipcode)
-    locationKey = HTTParty.get("http://dataservice.accuweather.com/locations/v1/postalcodes/search?apikey=WrWJozdXHdavGnA2GPSgC2C8x13aatgB&q=#{zipcode}")[0]["Key"]
+    locationKey = HTTParty.get("http://dataservice.accuweather.com/locations/v1/postalcodes/search?apikey=#{AUTH_DETAILS}&q=#{zipcode}")[0]["Key"]
   end
 
   def getTemps
     locationKey = getLocationKey(zipcode)
-    high_and_low = HTTParty.get("http://dataservice.accuweather.com/forecasts/v1/daily/1day/#{locationKey}?apikey=WrWJozdXHdavGnA2GPSgC2C8x13aatgB")["DailyForecasts"][0]["Temperature"]
+    high_and_low = HTTParty.get("http://dataservice.accuweather.com/forecasts/v1/daily/1day/#{locationKey}?apikey=#{AUTH_DETAILS}")["DailyForecasts"][0]["Temperature"]
     high = high_and_low["Maximum"]["Value"]
     low = high_and_low["Minimum"]["Value"]
-    current = HTTParty.get("http://dataservice.accuweather.com/currentconditions/v1/#{locationKey}?apikey=WrWJozdXHdavGnA2GPSgC2C8x13aatgB")[0]["Temperature"]["Imperial"]["Value"]
+    current = HTTParty.get("http://dataservice.accuweather.com/currentconditions/v1/#{locationKey}?apikey=#{AUTH_DETAILS}")[0]["Temperature"]["Imperial"]["Value"]
     temps = {high_temp: high, low_temp: low, current_temp: current}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  #get 'addresses/new'
-
-  root 'addresses#new'
+  get '/', to: redirect('/addresses/new')
 
   resources :addresses
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
